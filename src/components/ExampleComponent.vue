a24d0087f62f1a72f227868944b26c6584fb1601
Ted Vu <tedvu184@gmail.com>
2025-03-06 21:40:25 +1100

    explore quasar

--- src/components/ExampleComponent.vue
+++ src/components/ExampleComponent.vue
@@ -1,7 +1,5 @@
 <template>
-  <div>
-    <q-btn @click="$q.notify('My message')" color="primary" label="Show a notification" />
-  </div>
+  <div></div>
 </template>
 
 <script setup lang="ts"></script>
